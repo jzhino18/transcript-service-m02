de4369f9f33de8f0d00703cc3b3fcf5b8e9ef2af
Rob Simmons <robsimmons@gmail.com>
2025-12-20 22:36:50 -0500

    Transcript service?

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# little-possibility-spaces
-# transcript-server
-# transcript-server
+## Test-Driven Development
+This activity is intended to supplement the CS4530 lecture on test-driven development.
---
+++ src/transcript.service.spec.ts
@@ -0,0 +1,45 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { TranscriptDB, type TranscriptService } from './transcript.service.ts';
+
+let db: TranscriptService;
+beforeEach(() => {
+  db = new TranscriptDB();
+});
+
+describe('addStudent', () => {
+  it('should add a student to the database and return their id', () => {
+    expect(db.nameToIDs('blair')).toStrictEqual([]);
+    const id1 = db.addStudent('blair');
+    expect(db.nameToIDs('blair')).toStrictEqual([id1]);
+  });
+
+  it('should return an ID distinct from any ID in the database', () => {
+    // we'll add 3 students and check to see that their IDs are all different.
+    const id1 = db.addStudent('blair');
+    const id2 = db.addStudent('corey');
+    const id3 = db.addStudent('del');
+    expect(id1).not.toEqual(id2);
+    expect(id1).not.toEqual(id3);
+    expect(id2).not.toEqual(id3);
+  });
+
+  it('should permit adding a student w/ same name as an existing student', () => {
+    const id1 = db.addStudent('blair');
+    const id2 = db.addStudent('blair');
+    expect(id1).not.toEqual(id2);
+  });
+});
+
+describe('getTranscript', () => {
+  it('given the ID of a student, should return the student’s transcript', () => {
+    const id1 = db.addStudent('blair');
+    expect(db.getTranscript(id1)).not.toBeNull();
+  });
+
+  it('given the ID that is not the ID of any student, should throw an error', () => {
+    // in an empty database, all IDs are bad :)
+    // Note: the expression you expect to throw
+    // must be wrapped in a (() => ...)
+    expect(() => db.getTranscript(1)).toThrowError();
+  });
+});
